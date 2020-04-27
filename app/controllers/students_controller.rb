464613ec098dd10f61db521c0e6af609edece49c
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-04-27 09:50:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
   
   def create
-  Student.create(first_name: params[:student][:first_name], description: params[:post][:description])
+  Student.create(first_name: params[:student][:first_name], description: params[:student][:last_name])
   redirect_to posts_path
 end
   
